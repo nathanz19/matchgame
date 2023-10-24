56e497b65fd0e0e26f996daff2549784ef3cae2f
nathanz19 <114586082+nathanz19@users.noreply.github.com>
2023-10-24 14:32:20 +0000

    update

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function initialize(){
   
 }
 
+function assignRandomNumbers() {
+  const arr = [];
+  for (let i = 1; i <= 8; i++) {
+    arr.push(i, i);
+  }
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
