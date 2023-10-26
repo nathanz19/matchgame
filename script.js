f91580ee440ceeac79bd121a28be6ebf41b51a20
nathanz19 <114586082+nathanz19@users.noreply.github.com>
2023-10-26 19:08:07 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function initialize(){
-  
+let randomizedImages = assignRandomNumbers();
+let images = document.querySelectorAll("img");
+let totalNumberOfClicks = 0;
+let tempImageID = "";
+
+function initialize() {
+  assignImages();
+  console.log(randomizedImages);
+  console.log(images)
 }
 
 function assignRandomNumbers() {
@@ -7,9 +14,35 @@ function assignRandomNumbers() {
   for (let i = 1; i <= 8; i++) {
     arr.push(i, i);
   }
+
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
+}
+
+function assignImages() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].src = `${randomizedImages[i]}.png`;
+    images[i].addEventListener("click", revealImage);
+  }
+}
+
+function revealImage(event) {
+  var CurrentImage = document.getElementById(event.target.id);
+  console.log("Reveal Image Triggered");
+  totalNumberOfClicks++;
+  console.log(totalNumberOfClicks);
+  console.log(CurrentImage);
+  switch (totalNumberOfClicks % 2) {
+    case 0:
+      break;
+
+    case 1:
+      tempImageID = CurrentImage.id;
+      
+      break;
+  }
+
 }
